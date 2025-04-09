0461208ca60087e61934d8d6e5bd136778154aac
Neetsel <55947537+Neetsel@users.noreply.github.com>
2025-04-09 14:52:36 +0200

    modified next config js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+
+/* const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
@@ -10,6 +26,6 @@ const nextConfig = {
 
     return config;
   },
-};
+};*/
 
 module.exports = nextConfig;
